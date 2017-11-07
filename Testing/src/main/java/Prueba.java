72770c08c061130f7db1f2e401d141c2574533df
Sebastian Canonaco <sebastiancanonaco@gmail.com>
2017-11-07 12:36:50 -0300

    primer proyecto maven

---
+++ Testing/src/main/java/Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
---
+++ Testing/src/test/java/PruebaTest.java
@@ -0,0 +1,7 @@
+public class PruebaTest extends TestCase{
+
+    @Test
+    public void testSuma(){
+        assertEquals("sumo bien", 5, Prueba.suma(2,3))
+    }
+}
