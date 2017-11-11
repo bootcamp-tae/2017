dac17f6dd39eed31eec3bab56ba08f4805ea3554
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-11-11 13:07:17 -0300

    flight builder and flights home done

--- bootcamp-1/src/main/java/bootcamp/model/FlightBuilder.java
+++ bootcamp-1/src/main/java/bootcamp/model/FlightBuilder.java
@@ -1,4 +1,92 @@
 package bootcamp.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public final class FlightBuilder {
+
+    private final String origin;
+    private final String destination;
+    private final int dateDeparting;
+    private final int adults;
+    private final int children;
+
+
+    FlightBuilder(String origin, String destination,int dateDeparting, int adults, int children){
+        this.origin=origin;
+        this.destination=destination;
+        this.dateDeparting=dateDeparting;
+        this.adults=adults;
+        this.children=children;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDateDeparting() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        return LocalDate.now().plusDays(dateDeparting).format(format);
+    }
+
+    public int getAdults() {
+        return adults;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public static final class Builder {
+
+
+        private String origin;
+        private String destination;
+        private int dateDeparting;
+        private int adults;
+        private int children;
+
+        private Builder() {
+        }
+
+        public static Builder flightCriteria() {
+            return new Builder();
+        }
+
+        public Builder setOrigin(String origin) {
+            this.origin = origin;
+            return this;
+        }
+
+        public Builder setDestination(String destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        public Builder setDateDeparting(int dateDeparting) {
+            this.dateDeparting = dateDeparting;
+            return this;
+        }
+
+        public Builder setAdults(int adults) {
+            this.adults = adults;
+            return this;
+        }
+
+        public Builder setChildren(int children) {
+            this.children = children;
+            return this;
+        }
+
+        public FlightBuilder build() {
+            return new FlightBuilder(origin,destination,dateDeparting,adults,children);
+        }
+
+    }
+
+
 }
--- bootcamp-1/src/main/java/bootcamp/pages/landing/Flights.java
+++ bootcamp-1/src/main/java/bootcamp/pages/landing/Flights.java
@@ -1,6 +1,8 @@
 package bootcamp.pages.landing;
 
+import bootcamp.model.FlightBuilder;
 import bootcamp.pages.CheapTicketsPage;
+import bootcamp.pages.components.Flights.FlightResults;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -8,5 +10,34 @@ public class Flights extends CheapTicketsPage{
 
     @FindBy(id="flight-type-one-way-label")
     private WebElement btnOneWatFlight;
+
+    @FindBy(id="flight-origin")
+    private WebElement inputOrigin;
+
+    @FindBy(id="flight-destination")
+    private WebElement inputDestination;
+
+    @FindBy(id="flight-departing")
+    private WebElement inputDateDeparting;
+
+    @FindBy(id="flight-adults")
+    private WebElement inputAdults;
+
+    @FindBy(id="flight-children")
+    private WebElement inputChildrens;
+
+    @FindBy(id="search-button")
+    private WebElement searchBtn;
+
+    public FlightResults search(FlightBuilder builder){
+
+        type(inputOrigin, builder.getOrigin());
+        type(inputDestination, builder.getDestination());
+        enterDate(inputDateDeparting, builder.getDateDeparting());
+        selectByText(inputAdults, String.valueOf(builder.getAdults()));
+        selectByText(inputChildrens, String.valueOf(builder.getChildren()));
+        click(searchBtn);
+        return new FlightResults();
+    }
 
 }
