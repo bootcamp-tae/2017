ecf7d69a591b27390759accb555039916a3cafdd
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-11-17 00:03:45 -0300

    fixed

--- mobile/src/main/java/bootcamp/CalculatorPage.java
+++ mobile/src/main/java/bootcamp/CalculatorPage.java
@@ -3,7 +3,7 @@ package bootcamp;
 import io.appium.java_client.AppiumDriver;
 import mobile.MobilePage;
 
-public class CalculatorPage extends MobilePage {
+public abstract class CalculatorPage extends MobilePage {
 
     //with moto g 4 6.0.1
     protected static final String ANDROID_PREFIX = "com.android.calculator2:id/";
